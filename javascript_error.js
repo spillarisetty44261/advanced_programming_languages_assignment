38d5a7eef1567f0c8d8cb05364020a4e3732bacf
spillarisetty44261 <spillarisetty44261@ucumberlands.edu>
2026-05-22 23:14:32 -0400

    uploading syntax errors

---
+++ javascript_error.js
@@ -0,0 +1,12 @@
+// JavaScript: Syntax Error Demo - missing closing brace
+function calculateSum(arr) {
+    let total = o;
+    for (let num of arr) {
+        total += num;
+    }
+    return total;
+// missing closing brace here
+
+let numbers = [1, 2, 3, 4, 5];
+let result = calculateSum(numbers);
+console.log("Sum in JavaScript:", result);
